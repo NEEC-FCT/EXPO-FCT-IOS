0cc08edbb38807cc1a6517e207138dbee788f93a
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-02-22 23:10:06 +0000

    Base

---
+++ tabbedApp/Mapa.swift
@@ -0,0 +1,35 @@
+//
+//  SecondViewController.swift
+//  tabbedApp
+//
+//  Created by Sebastian Hette on 07.11.2016.
+//  Copyright © 2016 MAGNUMIUM. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Mapa: UIViewController {
+    
+    @IBOutlet weak var map: MKMapView!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        super.viewDidLoad()
+        let span:MKCoordinateSpan = MKCoordinateSpanMake( 0.008 , 0.008)
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(38.661307,  -9.207192)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        map.setRegion(region, animated: true)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ tabbedApp/Parceiros.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  tabbedApp
+//
+//  Created by Sebastian Hette on 07.11.2016.
+//  Copyright © 2016 MAGNUMIUM. All rights reserved.
+//
+
+import UIKit
+
+class Parceiros: UIViewController {
+    
+    @IBOutlet weak var webview: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL (string: "https://neec-fct.com/demoappp/equipa/team/partners.html");
+        let requestObj = NSURLRequest(url: url! as URL);
+        webview.loadRequest(requestObj as URLRequest);
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
