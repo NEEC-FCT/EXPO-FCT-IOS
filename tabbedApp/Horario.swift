4ece2783fa4b4ad2d630f88bceaf4729f1955fbc
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-02-23 00:30:30 +0000

    Horário tab done

---
+++ tabbedApp/Horario.swift
@@ -0,0 +1,33 @@
+//
+//  Horario.swift
+//  tabbedApp
+//
+//  Created by NEEC on 23/02/2019.
+//  Copyright © 2019 MAGNUMIUM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Horario: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL (string: "https://expofct.neec-fct.com/schedule/")
+        let requestObj = URLRequest(url: url!)
+        webView.loadRequest(requestObj)
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
