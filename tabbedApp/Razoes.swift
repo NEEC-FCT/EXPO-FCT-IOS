fcfbd3ac3ad2ac77865c17ee3623de6113b79411
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-02-23 00:22:28 +0000

    Razoes tab done

---
+++ tabbedApp/Razoes.swift
@@ -0,0 +1,33 @@
+//
+//  Razoes.swift
+//  tabbedApp
+//
+//  Created by NEEC on 23/02/2019.
+//  Copyright © 2019 MAGNUMIUM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Razoes: UIViewController {
+    
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = URL (string: "https://expofct.neec-fct.com/razoes/")
+        let requestObj = URLRequest(url: url!)
+        webView.loadRequest(requestObj)
+        
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
