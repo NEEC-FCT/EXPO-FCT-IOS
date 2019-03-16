91df3eec6b98b5e7431a1e322b20fb181fe52e0b
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-03-16 02:43:15 +0000

    Added FAB

--- tabbedApp/Mapa.swift
+++ tabbedApp/Mapa.swift
@@ -9,6 +9,7 @@
 
 import UIKit
 import MapKit
+import JJFloatingActionButton
 
 class Mapa: UIViewController {
     
@@ -22,6 +23,39 @@ class Mapa: UIViewController {
         let requestObj = URLRequest(url: url!)
         webView.loadRequest(requestObj)
        // webView.delegate = self
+        
+        let actionButton = JJFloatingActionButton()
+        
+        actionButton.addItem(title: "Equipa", image: UIImage(named: "team")?.withRenderingMode(.alwaysTemplate)) { item in
+            // do something
+        }
+        
+        actionButton.addItem(title: "Scan", image: UIImage(named: "qrcode")?.withRenderingMode(.alwaysTemplate)) { item in
+            DispatchQueue.main.async {
+                
+               
+                    let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                    let newViewController = storyBoard.instantiateViewController(withIdentifier: "reader")
+                    self.present(newViewController, animated: true, completion: nil)
+                
+                
+            }
+        }
+        
+        
+        view.addSubview(actionButton)
+       
+        view.addSubview(actionButton)
+        actionButton.translatesAutoresizingMaskIntoConstraints = false
+        if #available(iOS 11.0, *) {
+            actionButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+               actionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -65).isActive = true
+        } else {
+            // Fallback on earlier versions
+            actionButton.display(inViewController: self)
+        }
+     
+
        
         
     }
